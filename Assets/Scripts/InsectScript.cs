af8c55ab32ef4178a897a7c4f222d7baf83395c3
jarthudp <jarthur.patrocinio@gmail.com>
2024-03-16 17:11:55 -0400

    enemy script added
    the insects now spawn in a random range of time and goes to the assigned target

---
+++ Assets/Scripts/InsectScript.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InsectScript : MonoBehaviour
+{
+    
+    [SerializeField] float speed;
+    [SerializeField] float minimumDistance;
+
+    public Transform target;
+    private Rigidbody2D rb;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.Find("TargetFollowTest").transform;
+        //FindObjectOfType<PlayerMovement>().transform;
+        rb = GetComponent<Rigidbody2D>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector2.Distance(transform.position, target.position) > minimumDistance)
+        {
+            //moves character TO target
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 aimDirection = target.position - transform.position;
+        float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x)*Mathf.Rad2Deg-90f;
+        rb.rotation = aimAngle;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Destroy(this.gameObject);
+            //add 1 insect part to total
+        }
+    }
+}
